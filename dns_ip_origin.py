ab5cb53e3361b3c4ba96016f2d0570e51be605df
ybusel <yevgeniy.busel@akqa.com>
2016-07-13 15:23:37 -0700

    Add files via upload

---
+++ dns_ip_origin.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+import re
+
+try:
+    if sys.argv[1:]:
+        print "File: %s" % (sys.argv[1])
+        dnsfile = sys.argv[1]
+    else:
+        dnsfile = raw_input("Please enter dns file to parse: ")
+
+        file = open(dnsfile,"r")
+
+        for line in file.readlines():
+                regex = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",line)
+                if regex:
+                        print "%s\n" %regex
+#                       print line
+
+except IOError, (errno, strerror):
+        print "I/O Error(%s) : %s" % (errno, strerror)
+
